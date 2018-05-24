29dc1606cb0a4e2030290d5d6a25dfbb277b5d10
Nealsbo <evolution1708@gmail.com>
2018-05-24 13:43:26 +0300

    Update render.js

--- render.js
+++ render.js
@@ -12,7 +12,7 @@ function init(){
 	renderer = new THREE.WebGLRenderer();
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-	camera.position.set( 0, 1, 4 );
+	camera.position.set( 0, 1, 5 );
 
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
