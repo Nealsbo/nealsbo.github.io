bda6a32f5b0aa35027d911d39214144a011b8f01
Pavel Chernov <pavel.chernov@natcheztr.com>
2018-07-24 17:11:02 +0300

    Updated initial camera position

--- render.js
+++ render.js
@@ -21,9 +21,10 @@ function init(){
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color( 0xffffff );
 	renderer = new THREE.WebGLRenderer();
-
+	
+	var campos = new THREE.Vector3(4, 2, 4).normalize().multiplyScalar(5);
 	camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 0.1, 1000 );
-	camera.position.set( 0, 0, 5 );
+	camera.position.set( campos.x, campos.y, campos.z );
 
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
